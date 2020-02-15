c8cb07af6915e2637fe596d171a6a1e6404d325c
Nukepayload2 <1939357182@qq.com>
2020-02-15 20:49:54 +0800

    完善文档

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RgssWindowStyleSettings
+# RGSS 窗口调节器
 设置 RGSS Player 窗口样式
 
 支持的样式：
@@ -6,3 +6,18 @@
 - 是否可以改变大小
 
 内置窗口搜寻设置支持夜弦之音，已测试 1.00 版。对于其它游戏，需要更改进阶设置里的窗口名称格式。
+
+### 发行版下载
+https://gitee.com/nukepayload2/RgssWindowStyleMgrDocs/releases
+
+### 系统要求
+#### .NET Framework 版
+最低要求:
+- .NET Framework 4.6.2
+- Windows 10 版本 1607 (14393) 或更高、Windows 8.1 Update 3 或更高、Windows 7 SP1 中的一种，或者它们对应的服务器系统
+- x86 或者 x64 的 CPU
+- DirectX 11 API, 功能级别 9.3 的 GPU
+
+推荐:
+- .NET Framework 4.7.2 或更高
+- 最新稳定版的 Windows 10
--- 窗口调节器/MainWindow.xaml.vb
+++ 窗口调节器/MainWindow.xaml.vb
@@ -78,4 +78,8 @@ Class MainWindow
     Private Sub BtnViewSource_Click(sender As Object, e As RoutedEventArgs) Handles BtnViewSource.Click
         Process.Start("explorer", "https://github.com/Nukepayload2/RgssWindowStyleSettings")
     End Sub
+
+    Private Sub BtnHelpTitleFormat_Click(sender As Object, e As RoutedEventArgs) Handles BtnHelpTitleFormat.Click
+        Process.Start("explorer", "https://gitee.com/nukepayload2/RgssWindowStyleMgrDocs/blob/master/%E7%AA%97%E5%8F%A3%E5%90%8D%E7%A7%B0%E6%A0%BC%E5%BC%8F%E8%AF%B4%E6%98%8E.md")
+    End Sub
 End Class
