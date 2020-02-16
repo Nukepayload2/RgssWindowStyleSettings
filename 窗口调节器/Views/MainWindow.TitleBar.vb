f7a49a971850d4e4ac18a3da3ae474a9bdebdd14
Nukepayload2 <1939357182@qq.com>
2020-02-16 14:35:26 +0800

    整理文件夹，准备添加功能

---
+++ 窗口调节器/Views/MainWindow.TitleBar.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Reflection
+
+Partial Class MainWindow
+
+    Private Sub BtnAbout_Click(sender As Object, e As RoutedEventArgs) Handles BtnAbout.Click
+        Dim version = GetType(MainWindow).Assembly.
+            GetCustomAttribute(Of AssemblyFileVersionAttribute)?.Version
+        MsgBox($"版本: {version}
+作者: Nukepayload2
+禁止用于商业用途。如发此软件被贩卖，请举报店铺。", MsgBoxStyle.Information, "关于 " & Title)
+    End Sub
+
+    Private Sub BtnViewSource_Click(sender As Object, e As RoutedEventArgs) Handles BtnViewSource.Click
+        Process.Start("explorer", "https://github.com/Nukepayload2/RgssWindowStyleSettings")
+    End Sub
+
+    Private Sub BtnViewReleases_Click(sender As Object, e As RoutedEventArgs) Handles BtnViewReleases.Click
+        Process.Start("explorer", "https://gitee.com/nukepayload2/RgssWindowStyleMgrDocs/releases")
+    End Sub
+
+End Class
---
+++ 窗口调节器/Views/MainWindow.xaml.vb
@@ -0,0 +1,29 @@
+﻿Class MainWindow
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        TxtWindowNameFormat.Text = Application.Settings.WindowPattern
+        ExpBasicSettings.IsExpanded = True
+        DetectGameWindow(silent:=True)
+    End Sub
+
+    Private Async Sub ExpBasicSettings_Expanded(sender As Object, e As RoutedEventArgs) Handles ExpBasicSettings.Expanded
+        ContentScroll.VerticalScrollBarVisibility = ScrollBarVisibility.Disabled
+        ExpAdvancedSettings.IsExpanded = False
+        Await Task.Delay(100)
+        ContentScroll.VerticalScrollBarVisibility = ScrollBarVisibility.Auto
+    End Sub
+
+    Private Async Sub ExpAdvancedSettings_Expanded(sender As Object, e As RoutedEventArgs) Handles ExpAdvancedSettings.Expanded
+        ContentScroll.VerticalScrollBarVisibility = ScrollBarVisibility.Disabled
+        ExpBasicSettings.IsExpanded = False
+        Await Task.Delay(100)
+        ContentScroll.VerticalScrollBarVisibility = ScrollBarVisibility.Auto
+    End Sub
+
+    Private Sub ChkUseWasd_Checked(sender As Object, e As RoutedEventArgs) Handles ChkUseWasd.Checked
+
+    End Sub
+
+    Private Sub ChkUseWasd_Unchecked(sender As Object, e As RoutedEventArgs) Handles ChkUseWasd.Unchecked
+
+    End Sub
+End Class
