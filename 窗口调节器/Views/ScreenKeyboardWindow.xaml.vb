a734e07c8a4c4d4e7fd41fe6a15892ad7144dd98
Nukepayload2 <1939357182@qq.com>
2020-02-20 00:11:56 +0800

    改善十字键手感

--- 窗口调节器/Views/ScreenKeyboardWindow.xaml.vb
+++ 窗口调节器/Views/ScreenKeyboardWindow.xaml.vb
@@ -1,25 +1,8 @@
 ﻿Imports System.Runtime.InteropServices
 Imports System.Windows.Interop
-Imports Nukepayload2.Diagnostics
 Imports Nukepayload2.Diagnostics.Preview
 
 Public Class ScreenKeyboardWindow
-
-    Private Async Sub BtnPressUp_Click(sender As Object, e As RoutedEventArgs) Handles BtnPressUp.Click
-        Await SimulateKeyPressAsync(BtnPressUp, VirtualKey.Up)
-    End Sub
-
-    Private Async Sub BtnPressDown_Click(sender As Object, e As RoutedEventArgs) Handles BtnPressDown.Click
-        Await SimulateKeyPressAsync(BtnPressDown, VirtualKey.Down)
-    End Sub
-
-    Private Async Sub BtnPressLeft_Click(sender As Object, e As RoutedEventArgs) Handles BtnPressLeft.Click
-        Await SimulateKeyPressAsync(BtnPressLeft, VirtualKey.Left)
-    End Sub
-
-    Private Async Sub BtnPressRight_Click(sender As Object, e As RoutedEventArgs) Handles BtnPressRight.Click
-        Await SimulateKeyPressAsync(BtnPressRight, VirtualKey.Right)
-    End Sub
 
     Private Async Sub BtnPressX_Click(sender As Object, e As RoutedEventArgs) Handles BtnPressX.Click
         Await SimulateKeyPressAsync(BtnPressX, VirtualKey.X)
@@ -34,36 +17,31 @@ Public Class ScreenKeyboardWindow
         SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE).ToInt64 Or WS_EX_NOACTIVATE)
     End Sub
 
-    Private Sub BtnPressDown_PreviewMouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles BtnPressDown.PreviewMouseLeftButtonDown
-        Debug.WriteLine("Down press")
-    End Sub
-
-    Private Sub BtnPressDown_PreviewMouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles BtnPressDown.PreviewMouseLeftButtonUp
-        Debug.WriteLine("Down up")
-    End Sub
-
     Private Async Function SimulateKeyPressAsync(curButton As UIElement, CurKey As VirtualKey) As Task
         Dim gameWnd = RgssSingleWindowManager.GetGameWindow()
         If gameWnd Is Nothing Then
             Return
         End If
-        curButton.IsEnabled = False
-        Try
-            If Not gameWnd.IsForeground Then
-                gameWnd.Activate()
-                Debug.WriteLine("Game window activated")
-            End If
-            Await SendKeyPressToGameAsync(CurKey)
-            Debug.WriteLine("Send key " & CurKey.ToString)
-        Finally
-            curButton.IsEnabled = True
-        End Try
+        If Not gameWnd.IsForeground Then
+            gameWnd.Activate()
+            Debug.WriteLine("Game window activated")
+        End If
+        Await SendKeyPressToGameAsync(CurKey)
+        Debug.WriteLine("Send key " & CurKey.ToString)
     End Function
 
-    Private Sub SendKeyUp(key As VirtualKey)
+    Private Sub SendKey(key As VirtualKey, options As InjectedInputKeyOptions)
+        Dim gameWnd = RgssSingleWindowManager.GetGameWindow()
+        If gameWnd Is Nothing Then
+            Return
+        End If
+        If Not gameWnd.IsForeground Then
+            gameWnd.Activate()
+            Debug.WriteLine("Game window activated")
+        End If
         Dim keyInput As New InjectedInputKeyboardInfo With {
             .VirtualKey = key,
-            .KeyOptions = InjectedInputKeyOptions.KeyUp
+            .KeyOptions = options
         }
         Dim inputs As INPUT() = {
             New INPUT With {
@@ -74,16 +52,63 @@ Public Class ScreenKeyboardWindow
     End Sub
 
     Private Sub SendKeyDown(key As VirtualKey)
-        Dim keyInput As New InjectedInputKeyboardInfo With {
-            .VirtualKey = key,
-            .KeyOptions = InjectedInputKeyOptions.KeyUp
-        }
-        Dim inputs As INPUT() = {
-            New INPUT With {
-                .type = INPUT_KEYBOARD,
-                .u = New InputUnion With {.ki = keyInput}
-            }}
-        SendInput(inputs.Length, inputs, Marshal.SizeOf(Of INPUT))
+        SendKey(key, InjectedInputKeyOptions.None)
+    End Sub
+
+    Private Sub SendKeyUp(key As VirtualKey)
+        SendKey(key, InjectedInputKeyOptions.KeyUp)
+    End Sub
+
+    Private _upPressed As Boolean
+    Private Sub BtnPressUp_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles BtnPressUp.PreviewMouseLeftButtonDown
+        _upPressed = True
+        SendKeyDown(VirtualKey.Up)
+    End Sub
+
+    Private Sub BtnPressUp_MouseLeaveOrLeftButtonUp() Handles BtnPressUp.MouseLeave, BtnPressUp.PreviewMouseLeftButtonUp
+        If _upPressed Then
+            _upPressed = False
+            SendKeyUp(VirtualKey.Up)
+        End If
+    End Sub
+
+    Private _downPressed As Boolean
+    Private Sub BtnPressDown_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles BtnPressDown.PreviewMouseLeftButtonDown
+        _downPressed = True
+        SendKeyDown(VirtualKey.Down)
+    End Sub
+
+    Private Sub BtnPressDown_MouseLeaveOrLeftButtonUp() Handles BtnPressDown.MouseLeave, BtnPressDown.PreviewMouseLeftButtonUp
+        If _downPressed Then
+            _downPressed = False
+            SendKeyUp(VirtualKey.Down)
+        End If
+    End Sub
+
+    Private _rightPressed As Boolean
+    Private Sub BtnPressRight_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles BtnPressRight.PreviewMouseLeftButtonDown
+        _rightPressed = True
+        SendKeyDown(VirtualKey.Right)
+    End Sub
+
+    Private Sub BtnPressRight_MouseLeaveOrLeftButtonUp() Handles BtnPressRight.MouseLeave, BtnPressRight.PreviewMouseLeftButtonUp
+        If _rightPressed Then
+            _rightPressed = False
+            SendKeyUp(VirtualKey.Right)
+        End If
+    End Sub
+
+    Private _leftPressed As Boolean
+    Private Sub BtnPressLeft_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs) Handles BtnPressLeft.PreviewMouseLeftButtonDown
+        _leftPressed = True
+        SendKeyDown(VirtualKey.Left)
+    End Sub
+
+    Private Sub BtnPressLeft_MouseLeaveOrLeftButtonUp() Handles BtnPressLeft.MouseLeave, BtnPressLeft.PreviewMouseLeftButtonUp
+        If _leftPressed Then
+            _leftPressed = False
+            SendKeyUp(VirtualKey.Left)
+        End If
     End Sub
 
 End Class
